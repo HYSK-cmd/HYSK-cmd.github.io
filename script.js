d3a3a39c7d638d391b6704884988b05f5ddfffc4
Hoon <hoonyang00@gmail.com>
2025-12-19 22:49:01 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function sendMail(){
+    let params = {
+        user_email : document.getElementById("user_email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+    const msg = document.getElementById("msg");
+    const form = document.getElementById("contact_form");
+    /* Email Services ID, Template ID, Parameters*/
+    emailjs.send("service_bi067ki", "template_pgnmbfl", params).then(() => {
+        alert("Email Sent ✅");
+        msg.textContent = "Thank you ❤️";
+        form.reset();
+        setTimeout(() => {msg.textContent = "✉️";}, 10000);
+    });
+}
